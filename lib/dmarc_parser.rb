2d7ac6ca828ffb1ec7b62d7e4d13f99089c4edda
D Salko <dm.salko@gmail.com>
2020-07-04 14:07:44 -0400

    Initial commit, gem scaffolding.

---
+++ lib/dmarc_parser.rb
@@ -0,0 +1,6 @@
+require "dmarc_parser/version"
+
+module DmarcParser
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/dmarc_parser/version.rb
@@ -0,0 +1,3 @@
+module DmarcParser
+  VERSION = "0.1.0"
+end
